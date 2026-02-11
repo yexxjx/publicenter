4effc086e259bb4634f5df4af165674aa8793268
bbrrrvsg <myeonghui1813@gmail.com>
2026-02-11 17:49:36 +0900

    2026-02-11

--- src/securityincident/model/dao/IncidentDao.java
+++ src/securityincident/model/dao/IncidentDao.java
@@ -1,4 +1,66 @@
 package securityincident.model.dao;
 
+import securityincident.model.dto.IncidentDto;
+
+import java.sql.*;
+
 public class IncidentDao {
+
+    // 싱글톤 - 이한승
+    private IncidentDao(){connect();}
+    private static final IncidentDao instance = new IncidentDao();
+    public static IncidentDao getInstance(){
+        return instance;
+    }
+
+    // 데이터 베이스 연동  - 이한승
+    private String url = "jdbc:mysql://localhost:3306/crawlerDB";
+    private String user = "root";
+    private String password ="1234";
+
+    private Connection conn;
+
+    private void connect(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url,user,password);
+            System.out.println("데이터베이스 연동 성공");
+        }catch (Exception e){
+            System.out.println("데이터베이스 연동 실패");
+        }
+    }
+
+//    // 1. 보안사고 관리
+//    // 1.1. 보안사고 등록 DAO - 이한승
+//    public boolean incidentAddByAdmin(IncidentDto incidentDto){
+//        try{
+//            String sql = "INSERT INTO securityIncident " +
+//                    "(incidentYear, incidentDate, incidentType, " +
+//                    "incidentDescription, actionTaken, approvalStatus, companyId) " +
+//                    "VALUES (?, NOW(), ?, ?, ?, 'PENDING', ?)";
+//
+//            PreparedStatement ps = conn.prepareStatement(sql);
+//
+//
+//        }catch (SQLException e){
+//
+//        }
+//    }
+
+    // 이름 존재하는지 확인
+    public int getCompanyIdByName(String companyName) {
+        try {
+            String sql = "SELECT companyId FROM company WHERE companyName = ?";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, companyName);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                return rs.getInt("companyId");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
 }
--- src/securityincident/view/IncidentView.java
+++ src/securityincident/view/IncidentView.java
@@ -1,4 +1,53 @@
 package securityincident.view;
 
+import securityincident.controller.IncidentController;
+import securityincident.model.dao.IncidentDao;
+
+import java.util.Scanner;
+
 public class IncidentView {
-}
+
+    // 싱글톤 - 이한승
+    private IncidentView(){}
+    private static final IncidentView instance = new IncidentView();
+    public static IncidentView getInstance(){
+        return instance;
+    }
+
+    private IncidentController ic = IncidentController.getInstance();
+
+    // 스캐너
+    private Scanner scan = new Scanner(System.in);
+
+
+    //1. 보안사고관리
+    //1.1. 보안사고등록 view
+    public void incidentAddByAdmin() {
+
+        System.out.println("===== 사고 등록 =====");
+
+        System.out.print("기업명 입력 > ");
+        String companyName = scan.nextLine();
+
+        System.out.print("사고 년도 입력 > ");
+        String incidentYear = scan.nextLine();
+
+        System.out.print("사고 유형 입력 > ");
+        String incidentType = scan.nextLine();
+
+        System.out.print("사고 내용 입력 > ");
+        String incidentDescription = scan.nextLine();
+
+        System.out.print("조치 내용 입력 > ");
+        String actionTaken = scan.nextLine();
+
+
+
+        // 입력값만 Controller에 전달
+        ic.incidentAddByAdmin(companyName,incidentYear, incidentType, incidentDescription,actionTaken);
+
+
+    } // m end
+
+
+}//class end
