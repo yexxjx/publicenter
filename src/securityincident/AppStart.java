f2ec22ff0a932cc9829255d199c9b54a58b7d4c6
leehyeon2021 <leehyeon2021@gmail.com>
2026-02-11 16:06:39 +0900

    260211 admin MVC 1차 완

--- src/securityincident/AppStart.java
+++ src/securityincident/AppStart.java
@@ -1,4 +1,9 @@
 package securityincident;
+// 작업자 이태현,
+import securityincident.view.MainView;
 
 public class AppStart {
+    public static void main(String[] args) {
+        MainView.getInstance().index();
+    }
 }
--- src/securityincident/controller/AdminController.java
+++ src/securityincident/controller/AdminController.java
@@ -1,4 +1,29 @@
 package securityincident.controller;
 
+import securityincident.model.dao.AdminDao;
+
 public class AdminController {
+    private AdminController(){}
+    private static final AdminController instance = new AdminController();
+    public static AdminController getInstance(){return instance;}
+
+    private AdminDao ad = AdminDao.getInstance();
+
+    // +) 관리자 로그인 세션
+    private int loinSession = 0;
+    public int getLoinSession(){
+        return loinSession;
+    }
+
+    // 1. 관리자 로그인
+    public boolean adminLogin(String pw){
+        boolean result = ad.adminLogin(pw);
+        return result;
+    }
+
+    // 2. 관리자 로그아웃
+    public boolean adminLogout(){
+        loinSession = 0;
+        return true;
+    }
 }
