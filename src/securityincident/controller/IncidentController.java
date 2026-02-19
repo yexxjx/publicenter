1aa14bef646481a04d1ec0ae7ef606fc1f227702
leehyeon2021 <leehyeon2021@gmail.com>
2026-02-19 17:14:54 +0900

    260219 수정

--- src/securityincident/controller/IncidentController.java
+++ src/securityincident/controller/IncidentController.java
@@ -2,42 +2,17 @@ package securityincident.controller;
 
 import securityincident.model.dao.CompanyDao;
 import securityincident.model.dao.IncidentDao;
-import securityincident.model.dto.CompanyDto;
 import securityincident.model.dto.IncidentDto;
 
 import java.util.ArrayList;
 
 public class IncidentController {
-
-    // 싱글톤 - 이한승
+    // 싱글톤
     private IncidentController(){}
     private static final IncidentController instance = new IncidentController();
-    public static IncidentController getInstance(){
-        return instance;
-    }
-
+    public static IncidentController getInstance(){return instance;}
     private IncidentDao id = IncidentDao.getInstance();
 
-    // 1. 보안사고관리
-    // 보안사고 등록 controller
-    public boolean incidentAddByAdmin(String companyName,String incidentYear, String incidentType, String incidentDescription,String actionTaken){
-
-        // 1. 기업 존재 확인
-        int companyId = id.getCompanyIdByName(companyName);
-
-        // 2. 기업이 존재하지 않으면 등록 불가
-        if (companyId == -1) {
-            System.out.println("❌ 존재하지 않는 기업입니다. 기업을 먼저 등록해주세요.");
-            return false;
-        }
-
-        // 3. dto 객체 생성
-        IncidentDto incidentDto = new IncidentDto();
-        incidentDto.setIncidentYear(incidentYear);
-        incidentDto.setIncidentType(incidentType);
-        incidentDto.setIncidentDescription(incidentDescription);
-        incidentDto.setActionTaken(actionTaken);
-        incidentDto.setCompanyId(companyId);
     // 1. 보안사고관리
     // 보안사고 등록 controller
     public boolean incidentAddByAdmin(String companyName,String incidentYear, String incidentType, String incidentDescription,String actionTaken){
@@ -96,46 +71,8 @@ public class IncidentController {
     public ArrayList<IncidentDto> incidentFindByYear(String year){
         ArrayList<IncidentDto>db = id.incidentFindByYear(year);
         return db;
-    }
-
-        boolean result = id.incidentAddByAdmin(incidentDto);
-
-        return result;
     } // m end
 
-    // 보안사고삭제
-    public boolean incidentDelete(int incidentId){
-        boolean result = id.incidentDelete(incidentId);
-        return result;
-    }
-
-    // 보안사고조회
-    public ArrayList<IncidentDto> incidentFindAll(){
-        ArrayList<IncidentDto>db = id.incidentFindAll();
-        return db;
-    }
-
-    // 보안사고수정
-    public boolean incidentUpdate(int incidentId, String incidentYear, String incidentDate,
-                                  String incidentType, String incidentDescription, String actionTaken){
-
-        boolean result = id.incidentUpdate(
-                incidentId,
-                incidentYear,
-                incidentDate,
-                incidentType,
-                incidentDescription,
-                actionTaken
-        );
-        return result;
-
-    }
-
-    //연도별 보안 사고 검색
-     public ArrayList<IncidentDto> incidentFindByYear(String year){
-        ArrayList<IncidentDto>db = id.incidentFindByYear(year);
-        return db;
-     }
 
     // 유형 목록 가져오기
     public ArrayList<String> getIncidentTypeList(){
